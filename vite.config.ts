e5900bd3dc2fd98c4b445edba1d0321a683f9e41
小恐龙 <46342554+xiaokonglongya@users.noreply.github.com>
2023-07-01 07:50:45 +0000

    初始化

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueTsx from '@vitejs/plugin-vue-jsx'
+import UnoCSS from 'unocss/vite'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueTsx(),
+  UnoCSS()],
+  server:{
+    host:true
+  }
+})
